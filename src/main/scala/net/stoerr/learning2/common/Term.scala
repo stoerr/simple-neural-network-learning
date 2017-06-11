10e972bddd3d5afb48fe06a93f49d9b9fccadc3a
Hans-Peter Stoerr <www.stoerr.net>
2017-06-11 19:51:48 +0200

    evaluation of Term

--- src/main/scala/net/stoerr/learning2/common/Term.scala
+++ src/main/scala/net/stoerr/learning2/common/Term.scala
@@ -13,6 +13,12 @@ sealed trait Term {
   def /(o: Term): Term = Quotient(this, o)
 
   def normalize: Term = this
+
+  def immediateSubterms: Iterator[Term]
+
+  def subterms: Iterator[Term] = Iterator(this) ++ immediateSubterms
+
+  def variables = subterms.filter(_.isInstanceOf[Var])
 }
 
 /** Various term functions */
@@ -24,14 +30,27 @@ object Term {
   implicit def apply(name: Symbol): Var = Var(name)
 
   def apply[T <: Term](term: T): T = term
+
+  def eval(term: Term, valuation: Map[Symbol, Double]): Double = term match {
+    case Const(c) => c
+    case Var(n) => valuation(n)
+    case Sum(summands) => summands.map(eval(_, valuation)).sum
+    case Minus(val1, val2) => eval(val1, valuation) - eval(val2, valuation)
+    case Product(factors) => factors.map(eval(_, valuation)).product
+    case Quotient(val1, val2) => eval(val1, valuation) / eval(val2, valuation)
+  }
 }
 
 case class Const(value: Double) extends Term {
   override def toString: String = value.toString
+
+  override def immediateSubterms: Iterator[Term] = Iterator.empty
 }
 
 case class Var(name: Symbol) extends Term {
   override def toString: String = name.name
+
+  override def immediateSubterms: Iterator[Term] = Iterator.empty
 }
 
 case class Sum(summands: List[Term]) extends Term {
@@ -44,10 +63,14 @@ case class Sum(summands: List[Term]) extends Term {
     }
     if (flattened.isEmpty) this else if (flattened.length == 1) flattened(0) else Sum(flattened)
   }
+
+  override def immediateSubterms: Iterator[Term] = summands.toIterator
 }
 
 case class Minus(value1: Term, value2: Term) extends Term {
   override def toString: String = "(" + value1 + " - " + value2 + ")"
+
+  override def immediateSubterms: Iterator[Term] = Iterator(value1, value2)
 }
 
 case class Product(factors: List[Term]) extends Term {
@@ -60,8 +83,12 @@ case class Product(factors: List[Term]) extends Term {
     }
     if (flattened.isEmpty) this else if (flattened.length == 1) flattened(0) else Product(flattened)
   }
+
+  override def immediateSubterms: Iterator[Term] = factors.iterator
 }
 
 case class Quotient(value1: Term, value2: Term) extends Term {
   override def toString: String = "(" + value1 + " / " + value2 + ")"
+
+  override def immediateSubterms: Iterator[Term] = Iterator(value1, value2)
 }
--- src/test/scala/net/stoerr/learning2/common/TestTerm.scala
+++ src/test/scala/net/stoerr/learning2/common/TestTerm.scala
@@ -1,15 +1,22 @@
 package net.stoerr.learning2.common
 
-import scala.language.implicitConversions
-
+import net.stoerr.learning2.common.Term._
 import org.scalatest._
-import Term.apply
+
+import scala.language.implicitConversions
 
 class TestTerm extends FlatSpec with Matchers {
 
   "A Term" should "generate a sensible string representation" in {
     ('a + 1).toString should be("(a + 1.0)")
     ('a + 'b + 'c + (1 * Var('b) * 'c) / 5 - 7).toString should be("((a + b + c + ((1.0 * b * c) / 5.0)) - 7.0)")
+  }
+
+  it should "have an evaluation" in {
+    val t = 'a / 'e + 'b * 'c - 5
+    t.toString should be("(((a / e) + (b * c)) - 5.0)")
+    eval('a / 'e + 'b * 'c - 5, Map('a -> 3, 'b -> 5, 'c -> 7, 'e -> 1.5)) should be(3.0 / 1.5 + 5.0 * 7.0 - 5)
+    an[NoSuchElementException] should be thrownBy eval('a + 'b, Map('a -> 1))
   }
 
 }
